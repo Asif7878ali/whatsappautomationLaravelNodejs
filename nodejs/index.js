7b1de807cee73e4efbf9736ccfaaa7c0b0eba10d
AsifRiseofPower <aloasif31@gmail.com>
2024-10-09 23:44:14 +0530

    stsats

--- nodejs/index.js
+++ nodejs/index.js
@@ -6,10 +6,10 @@ const whatsappbot = require('./routes/whatsappBrowserAutomate.js');
 server.get('/automate/browser/whatsbot', async (req, res) => {
       try {
         const screenshot = await whatsappbot();
-        res.status(200).json({ screenshot, message: 'QR Code Created Successfully' });
+        res.status(200).json({ screenshot, message: 'QR Code Created Successfully', status:true });
       } catch (error) {
         console.log('failed');
-        res.status(400).json({ message: 'errror through' });
+        res.status(400).json({ message: 'errror through',  status:false });
       }
      
   });
